40b77e0aec517adb06e20012dcfd4241865047c1
lalmeida89 <lalmeida89@gmail.com>
2017-12-01 14:00:02 -0500

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+function getDataFromApi(artist, title, callback) {
+  //your code here
+}
+
+function displaySearchData(data) {
+  //your code here
+}
+
+function watchSubmit() {
+  //your code here
+}
+
+$(watchSubmit);
